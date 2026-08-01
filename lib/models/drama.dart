9a182c92ca0a52e1c5d0c0485f538da9b8767283
fizhxCore <fizhxedit@gmail.com>
2026-08-01 21:04:45 +0700

    Upload lib/models/drama.dart via zip-to-github tool

--- lib/models/drama.dart
+++ lib/models/drama.dart
@@ -10,6 +10,18 @@ class Actor {
     required this.karakter,
     required this.fotoUrl,
   });
+
+  Map<String, dynamic> toJson() => {
+        'nama': nama,
+        'karakter': karakter,
+        'fotoUrl': fotoUrl,
+      };
+
+  factory Actor.fromJson(Map<String, dynamic> json) => Actor(
+        nama: json['nama'] as String? ?? '',
+        karakter: json['karakter'] as String? ?? '',
+        fotoUrl: json['fotoUrl'] as String? ?? '',
+      );
 }
 
 class Drama {
@@ -48,6 +60,67 @@ class Drama {
     required this.posterUrl,
     required this.pemeran,
   });
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'judul': judul,
+        'judulAsli': judulAsli,
+        'sinopsis': sinopsis,
+        'genre': genre,
+        'negara': negara,
+        'platformStreaming': platformStreaming,
+        'channel': channel,
+        'jumlahEpisode': jumlahEpisode,
+        'status': status,
+        'tanggalTayang': tanggalTayang,
+        'ratingGlobal': ratingGlobal,
+        'sutradara': sutradara,
+        'penulis': penulis,
+        'posterUrl': posterUrl,
+        'pemeran': pemeran.map((a) => a.toJson()).toList(),
+      };
+
+  factory Drama.fromJson(Map<String, dynamic> json) => Drama(
+        id: json['id'] as String,
+        judul: json['judul'] as String? ?? '',
+        judulAsli: json['judulAsli'] as String? ?? '',
+        sinopsis: json['sinopsis'] as String? ?? '',
+        genre: (json['genre'] as List?)?.map((e) => e as String).toList() ?? [],
+        negara: json['negara'] as String? ?? '',
+        platformStreaming:
+            (json['platformStreaming'] as List?)?.map((e) => e as String).toList() ?? [],
+        channel: json['channel'] as String? ?? '',
+        jumlahEpisode: json['jumlahEpisode'] as int? ?? 0,
+        status: json['status'] as String? ?? 'ongoing',
+        tanggalTayang: json['tanggalTayang'] as String? ?? '',
+        ratingGlobal: (json['ratingGlobal'] as num?)?.toDouble() ?? 0,
+        sutradara: json['sutradara'] as String? ?? '',
+        penulis: json['penulis'] as String? ?? '',
+        posterUrl: json['posterUrl'] as String? ?? '',
+        pemeran: (json['pemeran'] as List?)
+                ?.map((e) => Actor.fromJson(e as Map<String, dynamic>))
+                .toList() ??
+            [],
+      );
+
+  Drama copyWith({String? sinopsis}) => Drama(
+        id: id,
+        judul: judul,
+        judulAsli: judulAsli,
+        sinopsis: sinopsis ?? this.sinopsis,
+        genre: genre,
+        negara: negara,
+        platformStreaming: platformStreaming,
+        channel: channel,
+        jumlahEpisode: jumlahEpisode,
+        status: status,
+        tanggalTayang: tanggalTayang,
+        ratingGlobal: ratingGlobal,
+        sutradara: sutradara,
+        penulis: penulis,
+        posterUrl: posterUrl,
+        pemeran: pemeran,
+      );
 }
 
 class UserListEntry {
